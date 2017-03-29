075a6814168808bee893ac7417e90ef0a1642c62
Richard Jones <richard@cottagelabs.com>
2017-03-29 16:52:46 +0100

    some initial functionality for managing the data in the bid

--- src/js/bidomatic.js
+++ src/js/bidomatic.js
@@ -14,8 +14,16 @@ var bidomatic = {
                 bidomatic.newTagsBrowser({
                     category: "dm.lhs"
                 }),
+                bidomatic.newAddForm({
+                    id: "addform",
+                    category: "dm.rhs"
+                }),
                 bidomatic.newContentViewer({
                     category: "dm.rhs"
+                }),
+                bidomatic.newAddButton({
+                    category: "dm.control",
+                    controls: "addform"
                 })
             ]
         };
@@ -30,6 +38,8 @@ var bidomatic = {
         this.historyData = whetstone.getParam(params.historyData, false);
         this.current = whetstone.getParam(params.current, false);
         this.history = whetstone.getParam(params.history, false);
+
+        this.filters = {};
 
         this.setCurrentData = function(params) {
             var data = whetstone.getParam(params.data, {});
@@ -75,6 +85,24 @@ var bidomatic = {
             }
 
             return parsed;
+        };
+
+        this.addContent = function(params) {
+            var content = params.content;
+            var tags = params.tags;
+            var parsedTags = this._parseTags({source: tags});
+            this.current.push({
+                tags: parsedTags,
+                content: content,
+                id: whetstone.uuid4()
+            });
+            this.cycle();
+        };
+
+        this.addFilters = function(params) {
+            var tag = params.tag;
+            this.filters["tag"] = tag;
+            this.cycle();
         };
 
         this.init();
@@ -228,7 +256,10 @@ var bidomatic = {
             var containerClass = whetstone.css_classes(this.namespace, "container");
             var componentClass = whetstone.css_classes(this.namespace, "component");
 
-            var frag = '<div class="content"><div class="' + containerClass + '"><div class="row"><div class="col-md-6">{{LHS}}</div><div class="col-md-6">{{RHS}}</div></div></div></div>';
+            var frag = '<div class="content"><div class="' + containerClass + '">\
+                    <div class="row"><div class="col-md-12">{{CONTROL}}</div></div>\
+                    <div class="row"><div class="col-md-3">{{LHS}}</div><div class="col-md-9">{{RHS}}</div></div></div>\
+                </div>';
 
             var lhsFrag = "";
             var lhs = this.application.category("dm.lhs");
@@ -242,8 +273,15 @@ var bidomatic = {
                 rhsFrag += '<div class="' + componentClass + '"><div id="' + rhs[i].id + '"></div></div>';
             }
 
+            var controlFrag = "";
+            var control = this.application.category("dm.control");
+            for (var i = 0; i < control.length; i++) {
+                controlFrag += '<div class="' + componentClass + '"><div id="' + control[i].id + '"></div></div>';
+            }
+
             frag = frag.replace(/{{LHS}}/g, lhsFrag);
             frag = frag.replace(/{{RHS}}/g, rhsFrag);
+            frag = frag.replace(/{{CONTROL}}/g, controlFrag);
             this.application.context.html(frag);
         };
 
@@ -266,9 +304,11 @@ var bidomatic = {
     TagsBrowser : function(params)
     {
         this.tags = {};
+        this.tagFilter = false;
 
         this.synchronise = function() {
             this.tags = {};
+            this.tagFilter = false;
 
             for (var i = 0; i < this.application.current.length; i++) {
                 var row = this.application.current[i];
@@ -284,7 +324,16 @@ var bidomatic = {
                     }
                 }
             }
+
+            if (this.application.filters.hasOwnProperty("tag")) {
+                this.tagFilter = this.application.filters.tag;
+            }
         };
+
+        this.addPathFilter = function(params) {
+            var path = params.path;
+            this.application.addFilters({tag: path});
+        }
     },
 
     newTagsBrowserRend : function(params)
@@ -296,22 +345,44 @@ var bidomatic = {
         this.namespace = "bidomatic_tagsbrowser";
 
         this.draw = function() {
-            var frag = this._drawLevel({context: this.component.tags});
+            var linkClass = whetstone.css_classes(this.namespace, "link", this);
+            var frag = this._drawLevel({context: this.component.tags, linkClass: linkClass, path: ""});
             this.component.context.html(frag);
+
+            var linkSelector = whetstone.css_class_selector(this.namespace, "link", this);
+            whetstone.on(linkSelector, "click", this, "tagSelected");
+        };
+
+        this.tagSelected = function(element) {
+            var path = $(element).attr("data-path");
+            this.component.addPathFilter({path: path});
         };
 
         this._drawLevel = function(params) {
+            var linkClass = params.linkClass;
+            var path = params.path;
+
             var frag = "<ul>";
             var context = params.context;
             var keys = Object.keys(context);
             keys.sort();
             for (var i = 0; i < keys.length; i++) {
                 var key = keys[i];
-                frag += "<li>" + whetstone.escapeHtml(key);
+                var subPath = key;
+                if (path !== "") {
+                    subPath = path + "/" + key;
+                }
+                var startBold = "";
+                var endBold = "";
+                if (this.component.tagFilter && this.component.tagFilter === subPath) {
+                    startBold = "<strong>";
+                    endBold = "</strong>";
+                }
+                frag += "<li><a href='#' class='" + linkClass + "' data-path='" + subPath + "'>" + startBold + whetstone.escapeHtml(key) + endBold + "</a>";
                 var children = context[key];
                 var ckeys = Object.keys(children);
                 if (ckeys.length > 0) {
-                    frag += this._drawLevel({context: children});
+                    frag += this._drawLevel({context: children, linkClass: linkClass, path: subPath});
                 }
                 frag += "</li>";
             }
@@ -331,20 +402,93 @@ var bidomatic = {
         return whetstone.instantiate(bidomatic.ContentViewer, params, whetstone.newComponent);
     },
     ContentViewer : function(params) {
-        var limit = whetstone.getParam(params.limit, 10);
+        this.limit = whetstone.getParam(params.limit, 10);
 
         this.entries = [];
 
         this.synchronise = function() {
             this.entries = [];
 
+            var unsorted = [];
+            var j = 0;
             for (var i = 0; i < this.application.current.length ; i++) {
-                if (i > limit) {
+                if (j > this.limit) {
                     break;
                 }
-                this.entries.push(this.application.current[i]);
+                var entry = this.application.current[i];
+                if (this._filter(entry)) {
+                    unsorted.push(entry);
+                }
+                j++;
+            }
+
+            if (!this.application.filters.hasOwnProperty("tag")) {
+                this.entries = unsorted;
+                return;
+            }
+
+            var sortMap = {};
+            for (var i = 0; i < unsorted.length; i++) {
+                var entry = unsorted[i];
+                var sortTag = this._sortingTag(entry);
+                if (!(sortTag in sortMap)) {
+                    sortMap[sortTag] = [];
+                }
+                sortMap[sortTag].push(i);
+            }
+            var sortTags = Object.keys(sortMap);
+            sortTags.sort();
+            for (var i = 0; i < sortTags.length; i++) {
+                var ids = sortMap[sortTags[i]];
+                for (var j = 0; j < ids.length; j++) {
+                    this.entries.push(unsorted[ids[j]]);
+                }
             }
         };
+
+        this._filter = function(entry) {
+            if (!this.application.filters.hasOwnProperty("tag")) {
+                return true;
+            }
+            var tagFilter = this.application.filters.tag;
+            for (var i = 0 ; i < entry.tags.length; i++) {
+                var tag = entry.tags[i];
+                if (whetstone.startswith(tag.path, tagFilter)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        this._sortingTag = function(entry) {
+            var possibles = [];
+            var tagFilter = false;
+            if (this.application.filters.hasOwnProperty("tag")) {
+                tagFilter = this.application.filters.tag;
+            }
+            for (var i = 0 ; i < entry.tags.length; i++) {
+                var tag = entry.tags[i];
+                if (tagFilter)
+                {
+                    if (tag.path === tagFilter && tag.hasOwnProperty("sequence")) {
+                        possibles.push(tag.path + parseInt(tag.sequence));
+                    } else if (whetstone.startswith(tag.path, tagFilter)) {
+                        possibles.push(tag.path);
+                    }
+                } else {
+                    if (tag.hasOwnProperty("sequence")) {
+                        possibles.push(tag.path + parseInt(tag.sequence));
+                    } else {
+                        possibles.push(tag.path);
+                    }
+                }
+            }
+            if (possibles.length > 0) {
+                possibles.sort();
+                return possibles[0];
+            }
+            return "";
+        }
     },
 
     newContentViewerRend : function(params) {
@@ -354,7 +498,7 @@ var bidomatic = {
         this.namespace = "bidomatic_contentviewer";
 
         this.draw = function() {
-            var entryClass = whetstone.css_classes(this.namespace, "component");
+            var entryClass = whetstone.css_classes(this.namespace, "component", this);
 
             var frag = "";
             for (var i = 0; i < this.component.entries.length; i++) {
@@ -366,13 +510,108 @@ var bidomatic = {
         };
     },
 
-    /////////////////////////////////////////////////////////////////////
 
-    newTextBlock : function(params) {
-        return whetstone.instantiate(bidomatic.TextBlock, params, whetstone.newComponent);
+    newAddButton : function(params) {
+        var my = {
+            renderer : bidomatic.newAddButtonRend()
+        };
+        var may = {
+            id : "addbutton"
+        };
+        params = whetstone.overlay(my, params, may);
+        return whetstone.instantiate(bidomatic.AddButton, params, whetstone.newComponent);
     },
-    TextBlock : function(params) {
-        this.content = whetstone.getParam(params.content, "");
-        this.id = whetstone.getParam(parans.id);
+    AddButton : function(params) {
+        this.controls = whetstone.getParam(params.controls, "addform");
+
+        this.toggleAddForm = function() {
+            var comp = this.application.getComponent({id: this.controls});
+            comp.toggleVisible();
+            comp.draw();
+        }
+    },
+
+    newAddButtonRend : function(params) {
+        return whetstone.instantiate(bidomatic.AddButtonRend, params, whetstone.newRenderer);
+    },
+    AddButtonRend : function(params) {
+        this.namespace = "bidomatic_addbutton";
+
+        this.draw = function() {
+            var componentClass = whetstone.css_classes(this.namespace, "component", this);
+            var buttonId = whetstone.css_id(this.namespace, "add", this);
+
+            var frag = '<div class="' + componentClass + '"><button type="button" id="' + buttonId + '" class="alert alert-success">+ Add</button></div>';
+            this.component.context.html(frag);
+
+            var buttonSelector = whetstone.css_id_selector(this.namespace, "add", this);
+            whetstone.on(buttonSelector, "click", this, "addClicked");
+        };
+
+        this.addClicked = function(element) {
+            this.component.toggleAddForm();
+        }
+    },
+
+
+    newAddForm : function(params) {
+        var my = {
+            renderer : bidomatic.newAddFormRend()
+        };
+        var may = {
+            id : "addform"
+        };
+        params = whetstone.overlay(my, params, may);
+        return whetstone.instantiate(bidomatic.AddForm, params, whetstone.newComponent);
+    },
+    AddForm : function(params) {
+        this.visible = whetstone.getParam(params.visible, false);
+
+        this.toggleVisible = function() {
+            this.visible = !this.visible;
+        };
+
+        this.addContent = function(params) {
+            this.application.addContent(params);
+        };
+    },
+
+    newAddFormRend : function(params) {
+        return whetstone.instantiate(bidomatic.AddFormRend, params, whetstone.newRenderer);
+    },
+    AddFormRend : function(params) {
+        this.namespace = "bidomatic_addform";
+
+        this.draw = function() {
+            if (!this.component.visible) {
+                this.component.context.html("");
+                return;
+            }
+
+            var componentClass = whetstone.css_classes(this.namespace, "component", this);
+            var textareaId = whetstone.css_id(this.namespace, "content", this);
+            var tagsId = whetstone.css_id(this.namespace, "tags", this);
+            var buttonId = whetstone.css_id(this.namespace, "add", this);
+
+            var frag = '<div class="' + componentClass + '">\
+                    <textarea name="' + textareaId + '" id="' + textareaId + '" placeholder="content" style="width:100%"></textarea>\
+                    <textarea name="' + tagsId + '" id="' + tagsId + '" placeholder="tags (X/Y:seq|Z:seq)" style="width: 100%"></textarea>\
+                    <button type="button" id="' + buttonId + '" class="alert alert-success">Add</button>\
+                </div>';
+            this.component.context.html(frag);
+
+            var buttonSelector = whetstone.css_id_selector(this.namespace, "add", this);
+            whetstone.on(buttonSelector, "click", this, "addClicked");
+        };
+
+        this.addClicked = function(element) {
+            var contentSelector = whetstone.css_id_selector(this.namespace, "content", this);
+            var tagsSelector = whetstone.css_id_selector(this.namespace, "tags", this);
+            var content = this.component.jq(contentSelector).val();
+            var tags = this.component.jq(tagsSelector).val();
+            this.component.addContent({content: content, tags: tags});
+        };
     }
+
+
 };
