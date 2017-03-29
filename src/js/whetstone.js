d1c3f9b9842002e6ee1e405a73b6398016ec85e0
Richard Jones <richard@cottagelabs.com>
2017-03-29 20:38:28 +0100

    add first pass at generic file download function

--- src/js/whetstone.js
+++ src/js/whetstone.js
@@ -663,5 +663,33 @@ var whetstone = {
 
     startswith : function(str, prefix) {
         return str.substring(0, prefix.length) === prefix;
+    },
+
+    download : function(params) {
+        var content = params.content;
+        var filename = params.filename;
+        var mimetype = params.mimetype;
+
+        var textFileAsBlob = new Blob([content], {type:mimetype});
+        var downloadLink = document.createElement("a");
+        downloadLink.download = filename;
+        downloadLink.innerHTML = "Download File";
+        if (window.webkitURL != null)
+        {
+            // Chrome allows the link to be clicked
+            // without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        }
+        else
+        {
+            // Firefox requires the link to be added to the DOM
+            // before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            // downloadLink.onclick = destroyClickedElement;
+            // downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+        }
+
+        downloadLink.click();
     }
 };
